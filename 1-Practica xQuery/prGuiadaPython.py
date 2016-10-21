abfee8e9c882f43faa5ace9747dd6ff812e4c8a5
gorco <antcal01@ucm.es>
2016-10-21 20:53:02 +0200

    Añadida declaración de autoria

--- 1-Practica xQuery/prGuiadaPython.py
+++ 1-Practica xQuery/prGuiadaPython.py
@@ -4,6 +4,11 @@
 # Ejercicio B, Sesion Guiada 1
 # Autores: Antonio Calvo Morata y Carlos Congosto Sandoval
 
+# Antonio Calvo Morata y Carlos Congosto Sandoval declaramos que esta solución es fruto exclusivamente de nuestro
+# trabajo personal. No hemos sido ayudados por ninguna otra persona ni hemos obtenido la solución de fuentes externas,
+# y tampoco hemos compartido nuestra solución con nadie. Declaramos además que no hemos realizado de manera deshonesta
+# ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los demás.
+
 import eulexistdb.db
 
 db = eulexistdb.db.ExistDB("http://localhost:8080/exist")
