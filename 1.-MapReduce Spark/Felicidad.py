d976b97e81a51bb7d98b3bfcca5a75353d88f768
gorco <antcal01@ucm.es>
2016-10-21 20:53:19 +0200

    Empezando práctica 1 MR y Spark

---
+++ 1.-MapReduce Spark/Felicidad.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+# Este fichero generado para la asignatura SGDI
+# Practica 1 MapReduce Y Spark
+# Autores: Antonio Calvo Morata y Carlos Congosto Sandoval
+
+# Antonio Calvo Morata y Carlos Congosto Sandoval declaramos que esta solución es fruto exclusivamente de nuestro
+# trabajo personal. No hemos sido ayudados por ninguna otra persona ni hemos obtenido la solución de fuentes externas,
+# y tampoco hemos compartido nuestra solución con nadie. Declaramos además que no hemos realizado de manera deshonesta
+# ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los resultados de los demás.
+
+from mrjob.job import MRJob
+
+class MRHappiness(MRJob):
+
+	# Fase MAP (line es una cadena de texto)
+    def mapper(self, key, line):
+        word = line.split()
+        if float(word[2]) < 2.0 and word[4] != "--":
+            yield 1, (word[0], word[2])
+
+	# Fase REDUCE (key es una cadena texto, values un generador de valores)
+    def reducer(self, key, values):
+        yield key, list(values)
+
+
+if __name__ == '__main__':
+    MRHappiness.run()
