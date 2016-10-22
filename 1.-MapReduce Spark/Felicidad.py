91701199ceb50fd044a79b66a4c6b57e2de21a7a
gorco <antcal01@ucm.es>
2016-10-22 15:04:01 +0200

    Añadida parte 3 del ejercicio Felicidad de MR

--- 1.-MapReduce Spark/Felicidad.py
+++ 1.-MapReduce Spark/Felicidad.py
@@ -12,6 +12,8 @@
 from mrjob.job import MRJob
 from operator import itemgetter
 
+# 1.- Implementar una tarea MapReduce en mrjob que resuelva este problema utilizando únicamente las fases mapper
+# y reducer
 class MRHappiness(MRJob):
 
 	# Fase MAP (line es una cadena de texto)
@@ -27,6 +29,34 @@ class MRHappiness(MRJob):
         for i in range(0,5):
             yield valuesList[i]
 
+# 2.-  Implementar una tarea MapReduce en mrjob que resuelva este problema utilizando las fases mapper y reducer,
+# y utilice estructuras estáticas para reducir el número de parejas emitidas.
+class MRHappinessStatic(MRJob):
+
+    def __init__(self, *args, **kwargs):
+        super(MRHappinessStatic, self).__init__(*args, **kwargs)
+        self.listValues = []
+
+	# Fase MAP (line es una cadena de texto)
+    def mapper(self, key, line):
+        word = line.split()
+        if float(word[2]) < 2.0 and word[4] != "--":
+            self.listValues.append((word[0], word[2]))
+
+    def mapper_final(self):
+        val = sorted(self.listValues, key=itemgetter(1), reverse=True)
+        size = len(val) if len(val) < 5 else 5
+        for i in range(0, size):
+            yield "uniqueKey", val[i]
+
+	# Fase REDUCE (key es una cadena texto, values un generador de valores)
+    def reducer(self, key, values):
+        valuesList = list(values)
+        valuesList = sorted(valuesList, key=itemgetter(1), reverse=True)
+        for i in range(0,5):
+            yield valuesList[i]
+
 
 if __name__ == '__main__':
     MRHappiness.run()
+    MRHappinessStatic.run()
