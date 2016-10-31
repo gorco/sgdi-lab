632f1d1f981ba1e2af762e0176f03d6db75f916d
Carlos Congosto <kko-tb_92@hotmail.com>
2016-10-31 18:38:32 +0100

    Avanzado spark
    Falta la media y para maximo y el minimo habría que buscar otra forma.
    Se lo he copiado a Viktor

--- 1.-MapReduce Spark/B-Datos Meteorologicos/Meteo_spark.py
+++ 1.-MapReduce Spark/B-Datos Meteorologicos/Meteo_spark.py
@@ -14,36 +14,51 @@ from doctest import _OutputRedirectingPdb
 
 from pysparkling import Context
 
-
 # sys.argv debe contener el nombre de fichero a procesar
 if len(sys.argv) != 3:
-  print "Falta algún fichero!"
-  exit(-1)
+    print "Falta algún fichero!"
+    exit(-1)
 
 # Creamos un contexto local y cargamos el fichero        
 sc = Context()
-list = []
+#list = []
 
 lines = sc.textFile(sys.argv[1])
 
 
 def function(x):
     day = x[0].split("/")
-    return [day[1]+"/"+day[0], x[8]]
+    return day[1] + "/" + day[0], x[8]
+
+
+
 
 
 datos = (
-    lines.map((lambda x: x.split(','))) # Dividimos en palabras y aplanamos
-    .filter(lambda x: x[0]!= "date-time") # borramos la primera linea
-    .map(function) #nos quedamos con la fecha y la batería
+    lines.map((lambda x: x.split(',')))  # Dividimos en palabras y aplanamos
+        .filter(lambda x: x[0] != "date-time")  # borramos la primera linea
+        .map(function)  # nos quedamos con la fecha y la batería
+       # .reduceByKey(lambda x,y: (x,y))
+        #.flatMap(lambda x: x[1])
 )
 
-# En lugar de almacenar en disco, recolectamos y mostramos por pantalla
-#output = datos.collectAsMap()
+datos2 = datos.reduceByKey(lambda x: list.append(x))
 
-output = datos.collect()
-print output
+#cambiar que esto es lo mismo que otro grupo
+datos_min = datos.reduceByKey(lambda x,y: min(x,y))
+datos_max = datos.reduceByKey(lambda x,y: max(x,y))
+datos_avg = datos.reduceByKey(lambda x,y: (x+y))
+
+# En lugar de almacenar en disco, recolectamos y mostramos por pantalla
+list = datos2.values()
+for l in list:
+    print l
+#utput = datos.collect()
+#print datos_avg.collect()
+#for d in list:
+ #   print d
+#print output
 #min = output.min()
-#max = output.max()
-#print "min: "+ min+ "\t max: "+ max
-#sc.stop()
+# max = output.max()
+# print "min: "+ min+ "\t max: "+ max
+# sc.stop()
