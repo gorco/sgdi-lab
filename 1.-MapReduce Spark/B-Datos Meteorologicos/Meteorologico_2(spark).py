5c1412e224f94b08e85b8889fbec7c7166d4c4c2
gorco <antcal01@ucm.es>
2016-11-05 12:04:47 +0100

    Corregido el ejercicio B.2 de spark

--- 1.-MapReduce Spark/B-Datos Meteorologicos/Meteorologico_2(spark).py
+++ 1.-MapReduce Spark/B-Datos Meteorologicos/Meteorologico_2(spark).py
@@ -13,45 +13,48 @@ import sys
 import unicodedata
 from pysparkling import Context
 
+def batteryByDate(x):
+    day = x[0].split("/")
+    return day[1] + "/" + day[0], (x[8], x[8], x[8], 1)
+
+def getMinMaxSum(x, y):
+    maxValue = float(max(float(x[0]), float(y[0])))
+    sumValue = float(x[1]) + float(y[1])
+    minValue = float(min(float(x[2]), float(y[2])))
+    count = float(x[3]) + float(y[3])
+    return (maxValue, sumValue, minValue, count)
+
+def calculateAverage(x):
+    key = x[0]
+    values = x[1] #(maxValue, sumValue, minValue, numValues)
+    return key, dict(max=values[0], avg=float(values[1])/float(values[3]), min=values[2])
+
+
 # sys.argv debe contener el nombre de fichero a procesar
-if len(sys.argv) != 3:
-    print "Falta algún fichero!"
+if len(sys.argv) < 2:
+    print "No se ha especificado fichero!"
     exit(-1)
+
+path = sys.argv[1]
+for i in range(2, len(sys.argv)):
+    path += ","+sys.argv[i]
 
 # Creamos un contexto local y cargamos el fichero        
 sc = Context()
 
-def function(x):
-    day = x[0].split("/")
-    return day[1] + "/" + day[0], x[8]
+lines = sc.textFile(path)
 
+datos = (
+    lines.map((lambda x: x.split(',')))  # Dividimos en palabras
+        .filter(lambda x: x[0] != "date-time")  # Descartamos la primera linea (nombre de las columnas)
+        .map(batteryByDate)  # nos quedamos con la fecha y la batería
+        .reduceByKey(getMinMaxSum) # Obtenemos el minimo, el maximo
+        .map(calculateAverage)
+        .sortByKey()
+)
 
-for file in range(1,3):#se recorren todas los argumentos de la llamada
-    lines = sc.textFile(sys.argv[file])
+output = datos.collect()
 
-    datos = (
-        lines.map((lambda x: x.split(',')))  # Dividimos en palabras
-            .filter(lambda x: x[0] != "date-time")  # borramos la primera linea
-            .map(function)  # nos quedamos con la fecha y la batería
-            .groupByKey()
-            .sortByKey()#ordenamos por fecha
-    )
+for entry in output:
+   print entry[0], entry[1]
 
-    output = datos.collect()
-
-    for o in output:
-        sumValues = 0
-        tam = len(o[1])
-        o[1].sort()
-        maxValue = o[1][-1]
-        minValue = o[1][0]
-        for valor in o[1]:
-            sumValues+=float(str(valor))
-
-
-
-        print o[0], "{'max': ",maxValue,"'avg':", sumValues/tam, "'min':",minValue ,"}"
-
-#datos_min = datos.reduceByKey(lambda x,y: min(x,y))#unimos con la misma clave y nos quedamos con el menor
-#datos_max = datos.reduceByKey(lambda x,y: max(x,y))#unimos con la misma clave y nos quedamos con el mayor
-# datosFinal = datos.collect()
