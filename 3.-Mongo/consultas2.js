46c11f47cfb8bb38b3aea6901fdf23618cedfa48
gorco <antcal01@ucm.es>
2016-12-13 16:04:05 +0100

    Añadidos ejercicios MR

--- 3.-Mongo/consultas2.js
+++ 3.-Mongo/consultas2.js
@@ -71,21 +71,94 @@ function agg4(){
   
 // 1.- Paises y numero de peliculas
 function mr1(){
-	/* */
+	return db.peliculas.mapReduce(
+		//mapper
+		function(){
+			this.pais.forEach(function(entry){emit(entry,1);});
+		}, 
+		//reducer
+		function(key,values){
+			return Array.sum(values);
+		},
+		//otros
+		{
+			out : "result"
+		}).find();
 }
 
 // 2.- Tipo de pelicula y usuarios de 'Emiratos Árabes Unidos' a los que les gusta
 function mr2(){
-	/* */
+	return db.usuarios.mapReduce(
+		//mapper
+		function(){
+			this.gustos.forEach(function(entry){emit(entry,1);});
+		}, 
+		//reducer
+		function(key,values){
+			return Array.sum(values);
+		},
+		//otros
+		{
+			query: { 
+				"direccion.pais": "Emiratos Árabes Unidos"
+			},
+			out : "result"
+		}).find();
 }
 
 // 3.- Pais - edad minima-maxima-media de mayores de 17 años
 function mr3(){
-	/* */
+	return db.usuarios.mapReduce(
+		//mapper
+		function(){
+			emit(this.direccion.pais,{avg:this.edad, min: this.edad, max: this.edad, count: 1});
+		}, 
+		//reducer
+		function(key,values){
+			var avg2 = 0
+			var count2 = 0
+			var min2 = Number.MAX_VALUE
+			var max2 = Number.MIN_VALUE
+			for(var i=1;i<values.length;i++) {
+				avg2 += values[i].avg
+				count2 += values[i].count
+				min2 = min2 < values[i].min ? min2 : values[i].min 
+				max2 = max2 > values[i].max ? max2 : values[i].max 
+			}
+			return {
+				avg: avg2,
+				min: min2,
+				max: max2,
+				count: count2
+			};
+		},
+		//otros
+		{
+			query: { 
+				edad: {$gt: 17}
+			},
+			finalize : function(key, reducedVal){
+				reducedVal.avg = reducedVal.avg / reducedVal.count
+				return reducedVal
+			},
+			out : "result"
+		}).find();
 }
 
 // 4.- Titulo de pelicula y numero de visualizaciones
 function mr4(){
-	/* */
+	return db.usuarios.mapReduce(
+		//mapper
+		function(){
+			this.visualizaciones.forEach(function(entry){emit(entry.titulo,1);});
+		}, 
+		//reducer
+		function(key,values){
+			return Array.sum(values);
+		},
+		//otros
+		{
+			out : "result"
+		}).find();
 }
 
