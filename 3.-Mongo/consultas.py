6db259c23a7fd95d12048b432343d918ae5223cf
Carlos Congosto <kko-tb_92@hotmail.com>
2016-12-14 13:58:20 +0100

    Todo cambiado con returns

--- 3.-Mongo/consultas.py
+++ 3.-Mongo/consultas.py
@@ -19,15 +19,13 @@ def usuario_peliculas(user_id, n):
     client = MongoClient()
     db = client['sgdi_pr3']
 
-    #Consulta
-    user = db.usuarios.find_one({"_id":user_id},{"visualizaciones":{"$slice":n}})
-    #Mostrar resultados de la consulta
-    for v in user['visualizaciones']:
-        print 'Titulo: ', v['titulo']
-        print 'Fecha: ', v['fecha']
-
+    query = {"_id":user_id}#Consulta
+    proy = {"visualizaciones":{"$slice": n}, "visualizaciones.fecha":1, "visualizaciones.titulo":1, "_id":0}#proyección
+    user = db.usuarios.find(query,proy).limit(n)
     #Cierre de la conexión
     client.close()
+
+    return user
 
 
 # 2. _id, nombre y apellidos de los primeros 'n' usuarios a los que les gusten 
@@ -38,14 +36,13 @@ def usuarios_gustos(gustos, n):
     client = MongoClient()
     db = client['sgdi_pr3']
 
-    # Consulta
-    users = db.usuarios.find({"gustos":{"$all":gustos}}).limit(5)
-    # Mostrar resultados de la consulta
-    for user in users:
-        print user['_id'], '\t', user['nombre'], ' ', user['apellido1'], ' ', user['apellido2']
+    query = {"gustos":{"$all":gustos}}# Consulta
+    proy = {'_id':1, 'nombre':1, 'apellido1':1, 'apellido2':1}#proyección
+    users = db.usuarios.find(query,proy).limit(5)
 
     # Cierre de la conexión
     client.close()
+    return users
     
 # 3. Numero de películas producidas (aunque sea parcialmente) en un pais
 # >>> num_peliculas( 'España' )
@@ -56,11 +53,10 @@ def num_peliculas(pais):
 
     # Consulta
     peliculas = db.peliculas.find({"pais": pais}).count()
-    # Mostrar resultados de la consulta
-    print 'Peliculas producidas en ', pais,': ',peliculas
 
     # Cierre de la conexión
     client.close()
+    return peliculas
 
     
 # 4. _id de los usuarios que viven en tipo de via y en un piso concreto.
@@ -71,12 +67,14 @@ def usuarios_via_num(tipo_via, numero):
     db = client['sgdi_pr3']
 
     # Consulta
-    users = db.usuarios.find({"direccion.tipo_via":tipo_via},{"numero":numero})
-    # Mostrar resultados de la consulta
-    for user in users:
-        print user['_id']
+    query = {"direccion.tipo_via":tipo_via, "direccion.piso":numero}
+    proy = {"_id":1}
+    users = db.usuarios.find(query,proy)
+
 
     client.close()
+    return users
+
 
 
 # 5. _id de usuario de un determinado sexo y edad en un rango
@@ -86,13 +84,14 @@ def usuario_sexo_edad( sexo, edad_min, edad_max ):
     client = MongoClient()
     db = client['sgdi_pr3']
     # Consulta
-    users = db.usuarios.find({'sexo': 'M','edad':{'$gte':edad_min, '$lte':edad_max}})
-    # Mostrar resultados de la consulta
-    for user in users:
-        print user['_id']
+    query = {'sexo': 'M','edad':{'$gte':edad_min, '$lte':edad_max}}
+    proy = {'_id':1}
+    users = db.usuarios.find(query,proy)
 
     # Cierre de la conexión
     client.close()
+    return users
+
 
 # 6. Nombre, apellido1 y apellido2 de los usuarios cuyos apellidos coinciden,
 # ordenado por edad ascendente
@@ -103,13 +102,13 @@ def usuarios_apellidos():
     db = client['sgdi_pr3']
 
     #Consulta
-    users = db.usuarios.find({'$where': 'this.apellido1 == this.apellido2'}).sort('edad',1)
-    # Mostrar resultados de la consulta
-    for u in users:
-        print u['nombre'],' ',u['apellido1'],' ',u['apellido2']
+    query = {'$where': 'this.apellido1 == this.apellido2'}
+    proy = {'nombre':1, 'apellido1':1, 'apellido2':1, '_id':0}
+    users = db.usuarios.find(query, proy).sort('edad',1)
 
     # Cierre de la conexión
     client.close()
+    return users
 
 
 # 7.- Titulo de las peliculas cuyo director tienen un nombre que empieza por
@@ -121,13 +120,13 @@ def pelicula_prefijo( prefijo ):
     db = client['sgdi_pr3']
 
     # Consulta
-    peliculas = db.peliculas.find({'director': {'$regex': '^'+prefijo}})
-    # Mostrar resultados de la consulta
-    for peli in peliculas:
-        print peli['titulo']
+    query = {'director': {'$regex': '^'+prefijo}}
+    proy = {'_id':0, 'titulo':1}
+    peliculas = db.peliculas.find(query, proy)
 
     # Cierre de la conexión
     client.close()
+    return peliculas
 
 
 # 8.- _id de usuarios con exactamente 'n' gustos cinematográficos, ordenados por
@@ -139,13 +138,13 @@ def usuarios_gustos_numero(n):
     db = client['sgdi_pr3']
 
     # Consulta
-    usuarios = db.usuarios.find({'gustos':{'$size':n}}).sort('edad',1)
-    # Mostrar resultados de la consulta
-    for usuario in usuarios:
-        print usuario['_id']
-
+    query = {'gustos':{'$size':n}}
+    proy = {'_id':1}
+    usuarios = db.usuarios.find(query, proy).sort('edad',1)
     # Cierre de la conexión
     client.close()
+    return usuarios
+
 
 # 9.- usuarios que vieron una determinada pelicula en un periodo concreto
 # >>> usuarios_vieron_pelicula( '583ef650323e9572e2812680', '2015-01-01', '2016-12-31' )
@@ -155,14 +154,11 @@ def usuarios_vieron_pelicula(id_pelicula, inicio, fin):
     db = client['sgdi_pr3']
 
     # Consulta
-    usuarios = db.usuarios.find({'visualizaciones':
-                                     {'$elemMatch': {'_id':ObjectId(id_pelicula),'fecha':{'$gte':inicio, '$lte':fin}}}})
-    # Mostrar resultados de la consulta
-    for user in usuarios:
-        print user['_id']
+    query = {'visualizaciones':
+                     {'$elemMatch': {'_id':ObjectId(id_pelicula),'fecha':{'$gte':inicio, '$lte':fin}}}}
+    proy = {'_id':1}
+    usuarios = db.usuarios.find(query, proy)
 
     # Cierre de la conexión
     client.close()
-
-
-
+    return usuarios
